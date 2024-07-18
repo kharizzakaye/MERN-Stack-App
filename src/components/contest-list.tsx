1439f324bc9f9e8e42d7ed5b3ccef32f4c432034
Khariza <kharizzakaye@gmail.com>
2024-07-18 20:44:50 +0800

    server side rendering

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,15 +1,13 @@
 import ContestPreview from "./contest-preview";
-import axios from "axios";
 import { useEffect, useState } from "react";
-import { API_SERVER_URL } from "../public-config";
-import { fetchContest } from "../api-client";
+import { fetchContests } from "../api-client";
 
 const ContestList = ({ initialContests }) => {
     const [contests, setContests] = useState(initialContests);
 
     useEffect(() => {
-        fetchContest().then((data) => {
-            setContests(data.contests);
+        fetchContests().then((contests) => {
+            setContests(contests);
         });
     }, []);
 
