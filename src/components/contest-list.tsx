ef412b3504aafc40154d92e6f4ce8930c666fec5
Khariza <kharizzakaye@gmail.com>
2024-07-18 19:40:46 +0800

    Move api to api-client file

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,6 +1,18 @@
 import ContestPreview from "./contest-preview";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_SERVER_URL } from "../public-config";
+import { fetchContest } from "../api-client";
 
-const ContestList = ({contests}) => {
+const ContestList = ({ initialContests }) => {
+    const [contests, setContests] = useState(initialContests);
+
+    useEffect(() => {
+        fetchContest().then((data) => {
+            setContests(data.contests);
+        });
+    }, []);
+
     return (
         <div className="contest-list">
             { contests.map((contest) => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app";
-import axios from "axios";
-import { API_SERVER_URL } from "./public-config";
 
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
-axios.get(`${API_SERVER_URL}/contests`).then((resp) => {
-    console.log(resp);
 
-    // delay render of UI until data has been fetched
-    root.render(<App initialData={{ contests: resp.data.contests }} />);
-});
 
-// root.render(<App/>);
+root.render(
+    <App initialData={{ contests: [] }} /> // start with an empty data
+); 
